3586d649630f77076f19ed77e3c5808495ff3718
beevik <beevik@yahoo.com>
2013-11-27 17:50:02 -0800

    Minor improvements to etree helper functions.

--- helpers.go
+++ helpers.go
@@ -33,50 +33,40 @@ var escapeTable = [...]byte{
 	0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
 	0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
 	0, 0, 1, 0, 0, 0, 2, 3, 0, 0, 0, 0, 0, 0, 0, 0,
-	0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 4, 0, 5, 0,
-	0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-	0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-	0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-	0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-	0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-	0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-	0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-	0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-	0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-	0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-	0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-	0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+	0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 4, 0, 5,
 }
 
 var substTable = [...][]byte{
-	{'&', 'q', 'u', 'o', 't', ';'}, // 1
-	{'&', 'a', 'm', 'p', ';'},      // 2
-	{'&', 'a', 'p', 'o', 's', ';'}, // 3
-	{'&', 'l', 't', ';'},           // 4
-	{'&', 'g', 't', ';'},           // 5
+	{'&', 'q', 'u', 'o', 't', ';'}, // 1 "
+	{'&', 'a', 'm', 'p', ';'},      // 2 &
+	{'&', 'a', 'p', 'o', 's', ';'}, // 3 '
+	{'&', 'l', 't', ';'},           // 4 <
+	{'&', 'g', 't', ';'},           // 5 >
 }
 
 // escape generates an escaped XML string.
 func escape(s string) string {
 	buf := make([]byte, 0, len(s))
 	for i := 0; i < len(s); i++ {
-		c := s[i]
-		subst := escapeTable[c]
-		if subst > 0 {
-			buf = append(buf, substTable[subst-1]...)
+		if c := s[i]; int(c) < len(escapeTable) && escapeTable[c] > 0 {
+			buf = append(buf, substTable[escapeTable[c]-1]...)
 		} else {
 			buf = append(buf, c)
 		}
 	}
-	return string(buf)
+	switch {
+	case len(s) == len(buf):
+		return s
+	default:
+		return string(buf)
+	}
 }
 
 // isWhitespace returns true if the byte slice contains only
 // whitespace characters.
 func isWhitespace(s string) bool {
 	for i := 0; i < len(s); i++ {
-		c := s[i]
-		if c != ' ' && c != '\t' && c != '\n' && c != '\r' {
+		if c := s[i]; c != ' ' && c != '\t' && c != '\n' && c != '\r' {
 			return false
 		}
 	}
@@ -85,7 +75,7 @@ func isWhitespace(s string) bool {
 
 var crsp = "\n                                                                                "
 
-// spaces returns a carriage return followed by n spaces. It's used
+// crSpaces returns a carriage return followed by n spaces. It's used
 // to generate XML indentations.
 func crSpaces(n int) string {
 	if n+1 > len(crsp) {
